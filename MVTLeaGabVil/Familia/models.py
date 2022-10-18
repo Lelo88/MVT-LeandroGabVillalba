73e7e418a54fb2fb433f526e8e41e71de9f566b3
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-10-18 10:33:36 -0300

    Creando el repositorio para mi primer MVT!

---
+++ MVTLeaGabVil/Familia/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiar(models.Model):
+    
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    #edad = models.IntegerField()
+    fecha_nac = models.DateField(null=True)
+    
---
+++ MVTLeaGabVil/Familia/views.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from django.shortcuts import render
+from .models import Familiar
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def agrega_familiar(request, nombre, apellido, fecha_nac):
+    
+    familiar = Familiar(nombre=nombre, apellido=apellido)    
+    
+    dia_nac = datetime.strptime(fecha_nac, '%Y-%m-%d').date()
+    
+    familiar.fecha_nac = dia_nac
+    
+    familiar.save()
+
+    plantilla=loader.get_template('agrega_familiar.html')    
+    documento=plantilla.render({"name": familiar.nombre, "surname": familiar.apellido, "birth_date": familiar.fecha_nac} )
+    
+    return HttpResponse(documento)
+
+def muestra_familia(request):
+    
+    familia = Familiar.objects.all()
+    
+    return render(request, 'listado_familiar.html',{"lista_familia": familia})
